67c6e05ee4ed2c35a82a735cd3459c9fa4f81b52
shivaamakes <shivaofficial1810@gmail.com>
2024-11-10 23:41:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.secret_key = binascii.hexlify(os.urandom(24)).decode()  # Needed for flash m
 db_config = {
     'host': 'freshbasketdb.czc20migq3tu.eu-north-1.rds.amazonaws.com',  # Your RDS endpoint
     'user': 'admin',  # Your DB username
-    'password': 'freshbasket',  # Your DB password
+    'password': 'freshbasket123',  # Your DB password
     'database': 'fresh'
 }
 
